b9c10bea84df6d0dc763475386fa00393dcafffa
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-11-14 21:27:44 +0200

    UsersCard

---
+++ src/UsersGrid/UserCard.js
@@ -0,0 +1,10 @@
+export default function UserCard() {
+    return (
+        <div className="user">
+            <h3>Ivan</h3>
+            <img srv="#" alt="user1"></img>
+            <p>bio</p>
+            <p>Email</p>
+        </div>
+    )
+}
--- src/UsersGrid/UsersGrid.js
+++ src/UsersGrid/UsersGrid.js
@@ -1,23 +1,17 @@
 import './UsersGrid.css';
+import UserCard from './UserCard';
 
 export default function UsersGrid() {
     return (
         <>
             <h1>Users List</h1>
             <div className="users_grid">
-
-                <div className="user">
-                    <h3>Ivan</h3>
-                    <img srv="#" alt="user1"></img>
-                    <p>bio</p>
-                    <p>Email</p>
-                </div>
-                <div className="user">
-                    <h3>Andrey</h3>
-                    <img src="#" alt="user2"></img>
-                    <p>bio</p>
-                    <p>Email</p>
-                </div>
+                   <UserCard></UserCard> 
+                   <UserCard></UserCard> 
+                   <UserCard></UserCard> 
+                   <UserCard></UserCard> 
+                   <UserCard></UserCard> 
+                   <UserCard></UserCard> 
             </div>
         </>
     )
