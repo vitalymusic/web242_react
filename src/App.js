b0f19f5e6db14d68ce5766c1edb4310e22c11d8b
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-11-14 21:06:46 +0200

    react1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Table from './Table';
+import UsersGrid from './UsersGrid/UsersGrid'
+
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello React!!!</h1>
       <Table></Table>
+      <UsersGrid />
     </div>
   );
 }
--- src/UsersGrid/UsersGrid.js
+++ src/UsersGrid/UsersGrid.js
@@ -3,15 +3,16 @@ import './UsersGrid.css';
 export default function UsersGrid(){
     return(
         <div className="users_grid">
+            <h1>Users List</h1>
             <div className="user">
                 <h3>Ivan</h3>
-                <img srv="#"></img>
+                <img srv="#" alt="user1"></img>
                 <p>bio</p>
                 <p>Email</p>  
             </div>
             <div className="user">
                 <h3>Andrey</h3>
-                <img src="#"></img>
+                <img src="#" alt="user1"></img>
                 <p>bio</p>
                 <p>Email</p>  
             </div>
